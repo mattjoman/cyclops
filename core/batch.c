3811df7d7aff321e7cef152cfc3522cee302f9e7
Matthew Penney <matt@matthewpenney.net>
2026-04-28 00:54:15 +0100

    Can now do parameter sweeps from the cli

--- core/batch.c
+++ core/batch.c
@@ -32,6 +32,11 @@ static batch_conf_t *init_batch_conf(unsigned long long warmup_runs,
     return cfg;
 }
 
+static void destroy_batch_conf(batch_conf_t *cfg)
+{
+    free(cfg);
+}
+
 static double *alloc_double_array(unsigned long long length)
 {
     double *array = calloc(length, sizeof(double));
@@ -154,26 +159,194 @@ static void process_perf_ratio_data(batch_conf_t *cfg,
     }
 }
 
-void run_batch(unsigned long long warmup_runs,
-               unsigned long long batch_runs,
-               workload_t *wl,
-               const metric_grp_t *mg,
-               const char *output_file_name)
+static void run_batch(batch_conf_t *cfg,
+                      batch_data_t *batch_data,
+                      bool write_batch_to_csv,
+                      unsigned long long batch_no)
 {
-    batch_conf_t *cfg = init_batch_conf(warmup_runs, batch_runs, wl, mg);
-    batch_data_t *batch_data = init_batch_data(cfg);
-    const metric_backend_t *backend = get_backend(mg->backend);
+    const metric_backend_t *backend = get_backend(cfg->mg->backend);
 
-    wl->init(wl);
-    backend->bench_func(cfg, batch_data, wl->workload);
-    wl->clean();
+    cfg->wl->init(cfg->wl);
+    backend->bench_func(cfg, batch_data, cfg->wl->workload);
+    cfg->wl->clean();
 
     process_perf_counter_data(cfg, batch_data);
     process_perf_ratio_data(cfg, batch_data);
 
-    batch_to_csv(cfg, batch_data, output_file_name, true);
+    if (write_batch_to_csv) {
+        batch_to_csv(cfg, batch_data, batch_no);
+    }
     run_report(cfg, batch_data);
+}
 
-    destroy_batch_data(batch_data);
-    free(cfg);
+static metric_data_t *batch_get_metric_data(batch_data_t *data,
+                                            metric_id_t metric_id)
+{
+    for (int i = 0; i < data->n_raw; i++) {
+        if (data->raw_data[i].metric_id == metric_id) {
+            return &data->raw_data[i];
+        }
+    }
+    for (int i = 0; i < data->n_derived; i++) {
+        if (data->derived_data[i].metric_id == metric_id) {
+            return &data->derived_data[i];
+        }
+    }
+    return NULL;
+}
+
+/*** PARAM SWEEP *************************************************************/
+
+/*
+ * Calculates the number of batches needed to perform the parameter sweep.
+ */
+static unsigned long long ps_n_batches(param_sweep_t *ps)
+{
+    unsigned long long low = strtoull(ps->wl_param_low, NULL, 10);
+    unsigned long long high = strtoull(ps->wl_param_high, NULL, 10);
+    unsigned long long step = strtoull(ps->wl_param_step, NULL, 10);
+
+    unsigned long long diff = high - low;
+    unsigned long long mod = diff % step;
+
+    if (mod != 0) {
+        return (diff / step) + 2;
+    }
+    return (diff / step) + 1;
+}
+
+static param_sweep_t *init_param_sweep(workload_t *wl,
+                                       const metric_grp_t *mg,
+                                       char *wl_param_key,
+                                       char *wl_param_low,
+                                       char *wl_param_high,
+                                       char *wl_param_step)
+{
+    param_sweep_t *ps = NULL;
+    if (!(ps = calloc(1, sizeof(param_sweep_t)))) {
+        perror("Failed to allocate memory for param_sweep_t struct");
+        exit(1);
+    }
+
+    ps->wl = wl;
+    ps->mg = mg;
+
+    ps->wl_param_key = wl_param_key;
+    ps->wl_param_low = wl_param_low;
+    ps->wl_param_high = wl_param_high;
+    ps->wl_param_step = wl_param_step;
+
+    ps->n_batches = ps_n_batches(ps);
+
+    if (!(ps->data = calloc(mg->n_metrics, sizeof(ps_data_t)))) {
+        perror("Failed to allocate memory for ps_data_t array");
+        exit(1);
+    }
+
+    for (int i = 0; i < mg->n_metrics; i++) {
+        ps->data[i].batch_vals = calloc(ps->n_batches,
+                                        sizeof(ps_batch_data_t));
+        if (!ps->data[i].batch_vals) {
+            perror("Failed to allocate memory for ps_batch_data_t array");
+            exit(1);
+        }
+
+        ps->data[i].metric_id = mg->metrics[i];
+    }
+
+    return ps;
+}
+
+static void destroy_param_sweep(param_sweep_t *ps)
+{
+    for (int i = 0; i < ps->mg->n_metrics; i++) {
+        free(ps->data[i].batch_vals);
+        ps->data[i].batch_vals = NULL;
+    }
+
+    free(ps->data);
+    ps->data = NULL;
+}
+
+/* n is zero-based */
+static unsigned long long ps_get_nth_param_val(param_sweep_t *ps,
+                                               unsigned long long n)
+{
+    unsigned long long low = strtoull(ps->wl_param_low, NULL, 10);
+    unsigned long long high = strtoull(ps->wl_param_high, NULL, 10);
+    unsigned long long step = strtoull(ps->wl_param_step, NULL, 10);
+
+    if (n == ps->n_batches) {
+        return high;
+    }
+    return low + (n * step);
+}
+
+static void run_param_sweep(param_sweep_t *ps,
+                            unsigned long long warmup_runs,
+                            unsigned long long batch_runs,
+                            bool write_batches_to_csv)
+{
+    static char param_val_buf[64];
+
+    for (unsigned long long i = 0; i < ps->n_batches; i++) {
+
+        unsigned long long param_val = ps_get_nth_param_val(ps, i);
+        snprintf(param_val_buf, sizeof(param_val_buf), "%llu", param_val);
+
+        wl_reset_param(ps->wl, ps->wl_param_key, param_val_buf);
+
+        /* init batch */
+        batch_conf_t *batch_cfg = init_batch_conf(warmup_runs,
+                                                  batch_runs,
+                                                  ps->wl,
+                                                  ps->mg);
+        batch_data_t *batch_data = init_batch_data(batch_cfg);
+
+        /* run batch */
+        run_batch(batch_cfg, batch_data, write_batches_to_csv, i);
+
+        /* extract aggregate batch data for each metric */
+        for (int m = 0; m < ps->mg->n_metrics; m++) {
+            metric_data_t *batch_metric_data = batch_get_metric_data(
+                                            batch_data, ps->data[m].metric_id);
+            assert(batch_metric_data);
+            ps->data[m].batch_vals[i].agg = batch_metric_data->agg;
+            ps->data[m].batch_vals[i].param_sweep_val = param_val;
+        }
+    }
+
+    /* write to csv */
+}
+
+void run_cyclops(unsigned long long warmup_runs,
+                 unsigned long long batch_runs,
+                 workload_t *wl,
+                 const metric_grp_t *mg,
+                 char *wl_param_key,
+                 char *wl_param_low,
+                 char *wl_param_high,
+                 char *wl_param_step,
+                 bool write_batches_to_csv)
+{
+    if (wl_param_key) {
+        param_sweep_t *ps = init_param_sweep(wl,
+                                             mg,
+                                             wl_param_key,
+                                             wl_param_low,
+                                             wl_param_high,
+                                             wl_param_step);
+        run_param_sweep(ps, warmup_runs, batch_runs, write_batches_to_csv);
+        param_sweep_to_csv(ps);
+        destroy_param_sweep(ps);
+    } else {
+        batch_conf_t *batch_cfg = init_batch_conf(warmup_runs,
+                                                  batch_runs,
+                                                  wl,
+                                                  mg);
+        batch_data_t *batch_data = init_batch_data(batch_cfg);
+        run_batch(batch_cfg, batch_data, write_batches_to_csv, 0);
+        destroy_batch_conf(batch_cfg);
+        destroy_batch_data(batch_data);
+    }
 }
--- include/batch.h
+++ include/batch.h
@@ -1,6 +1,8 @@
 #ifndef BATCH_H
 #define BATCH_H
 
+#include <stdbool.h>
+
 #include "./microbench.h"
 #include "./metric.h"
 #include "./workload.h"
@@ -32,10 +34,42 @@ typedef struct batch_data {
     metric_data_t *derived_data;
 } batch_data_t;
 
-void run_batch(unsigned long long warmup_runs,
-               unsigned long long batch_runs,
-               workload_t *wl,
-               const metric_grp_t *mg,
-               const char *output_file_name);
+/*** PARAM SWEEP *************************************************************/
+
+typedef struct {
+    unsigned long long param_sweep_val;
+    double_agg_t agg;
+} ps_batch_data_t;
+
+typedef struct {
+    metric_id_t metric_id;
+    ps_batch_data_t *batch_vals;
+} ps_data_t;
+
+typedef struct {
+
+    unsigned long long warmup_runs;
+    unsigned long long batch_runs;
+    workload_t *wl;
+    const metric_grp_t *mg;
+
+    char *wl_param_key;
+    char *wl_param_low;
+    char *wl_param_high;
+    char *wl_param_step;
+
+    unsigned long long n_batches;
+    ps_data_t *data;
+} param_sweep_t;
+
+void run_cyclops(unsigned long long warmup_runs,
+                 unsigned long long batch_runs,
+                 workload_t *wl,
+                 const metric_grp_t *mg,
+                 char *wl_param_key,
+                 char *wl_param_low,
+                 char *wl_param_high,
+                 char *wl_param_step,
+                 bool write_batches_to_csv);
 
 #endif
