4275f4d155735fedffc496511bac8c9ce619bf63
Matthew Penney <matt@matthewpenney.net>
2026-05-16 20:57:52 +0100

    Assert that backends are consistent within groups

--- tests/unit/metric_grp.validate_mg_backends.c
+++ tests/unit/metric_grp.validate_mg_backends.c
@@ -5,7 +5,16 @@
 
 int main()
 {
-    assert(0 == 0);
+    MetricGrpRegistry *mg_registry = mg_registry_get_registry();
+    assert(mg_registry != NULL);
+
+    for (size_t i = 0; i < mg_registry->n_registered; i++) {
+        metric_grp_t *mg = mg_registry->registry[i];
+        for (int j = 0; j < mg->n_metrics; j++) {
+            const metric_t *m = get_metric_by_id(mg->metrics[j]);
+            assert(mg->backend == m->backend);
+        }
+    }
 
     return 0;
 }
