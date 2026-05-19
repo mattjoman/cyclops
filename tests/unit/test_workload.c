c5455266c85187def11f7b1857bc0d1d198841b3
Matthew Penney <matt@matthewpenney.net>
2026-05-19 23:33:23 +0100

    Add subtest for testing public workload functions

--- tests/unit/test_workload.c
+++ tests/unit/test_workload.c
@@ -3,7 +3,7 @@
 
 #include "../include/workload.h"
 
-int main()
+void test1()
 {
     registry_t *reg = registry_get_reg_by_id(REG_ID_WORKLOAD);
     workload_t *wl;
@@ -18,6 +18,26 @@ int main()
         /* n_params and params must both be 0/NULL or both not 0/NULL */
         assert((wl->n_params == 0) == (wl->params == NULL));
     }
+}
+
+void test2()
+{
+    workload_t *wl;
+
+    wl = wl_get_by_name("NOT_A_REAL_WORKLOAD");
+    assert(wl == NULL);
+
+    wl = wl_get_by_name(NULL);
+    assert(wl == NULL);
+
+    wl = wl_get_by_name("STRIDED_ARRAY");
+    assert(wl != NULL);
+}
+
+int main()
+{
+    test1();
+    test2();
 
     return 0;
 }
