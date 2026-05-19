e2c36bba8ccef323244f9b59648a01b1eaaeceef
Matthew Penney <matt@matthewpenney.net>
2026-05-19 15:08:57 +0100

    Migrate metric backends to central registry

--- core/metric_grp/backend.c
+++ core/metric_grp/backend.c
@@ -4,33 +4,13 @@
 #include "../../include/metric_grp.h"
 #include "./backend.h"
 
-static metric_backend_registry_t metric_be_registry = {
-    .n_registered = 0,
-    .registry = NULL,
-};
-
-metric_backend_registry_t *metric_be_registry_get_registry(void)
-{
-    return &metric_be_registry;
-}
-
-void metric_be_register(metric_backend_t *be)
-{
-    metric_backend_registry_t *reg = metric_be_registry_get_registry();
-
-    reg->registry = realloc(reg->registry,
-                            (reg->n_registered + 1)
-                            * sizeof(metric_backend_t *));
-    reg->registry[reg->n_registered++] = be;
-}
-
 metric_backend_t *metric_backend_get(metric_backend_id_t id)
 {
-    metric_backend_registry_t *reg = metric_be_registry_get_registry();
+    registry_t *reg = registry_get_reg_by_id(REG_ID_METRIC_BACKEND);
     metric_backend_t *metric_be;
 
     for (size_t i = 0; i < reg->n_registered; i++) {
-        metric_be = metric_be_registry.registry[i];
+        metric_be = (metric_backend_t *)reg->registry[i];
 
         if (metric_be->id == id) {
             return metric_be;
--- core/metric_grp/backend.h
+++ core/metric_grp/backend.h
@@ -3,18 +3,11 @@
 
 #include "../../include/metric_grp.h"
 
-typedef struct {
-    size_t n_registered;
-    metric_backend_t **registry;
-} metric_backend_registry_t;
-
-metric_backend_registry_t *metric_be_registry_get_registry(void);
-
-void metric_be_register(metric_backend_t *backend);
-
 #define REGISTER_BACKEND(be_ptr) \
-    static void __attribute((constructor)) register_be(void) { \
-        metric_be_register(be_ptr); \
+    static void __attribute((constructor)) _registry_register_object(void) { \
+        registry_register_object((void *)be_ptr, REG_ID_METRIC_BACKEND); \
     }
+
+metric_backend_t *metric_backend_get(metric_backend_id_t id);
 
 #endif
