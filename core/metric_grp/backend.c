092fa793b0f51e30cf5afdbff0c8bcd9b1a46c68
Matthew Penney <matt@matthewpenney.net>
2026-05-19 12:22:29 +0100

    Access registry via a getter rather than directly

--- core/metric_grp/backend.c
+++ core/metric_grp/backend.c
@@ -9,27 +9,33 @@ static metric_backend_registry_t metric_be_registry = {
     .registry = NULL,
 };
 
-void metric_be_register(metric_backend_t *be)
-{
-    metric_be_registry.registry = realloc(metric_be_registry.registry,
-                                          (metric_be_registry.n_registered + 1)
-                                          * sizeof(metric_backend_t *));
-    metric_be_registry.registry[metric_be_registry.n_registered++] = be;
-}
-
 metric_backend_registry_t *metric_be_registry_get_registry(void)
 {
     return &metric_be_registry;
 }
 
+void metric_be_register(metric_backend_t *be)
+{
+    metric_backend_registry_t *reg = metric_be_registry_get_registry();
+
+    reg->registry = realloc(reg->registry,
+                            (reg->n_registered + 1)
+                            * sizeof(metric_backend_t *));
+    reg->registry[reg->n_registered++] = be;
+}
+
 metric_backend_t *metric_backend_get(metric_backend_id_t id)
 {
-    for (size_t i = 0; i < metric_be_registry.n_registered; i++) {
-        metric_backend_t *metric_be = metric_be_registry.registry[i];
+    metric_backend_registry_t *reg = metric_be_registry_get_registry();
+    metric_backend_t *metric_be;
+
+    for (size_t i = 0; i < reg->n_registered; i++) {
+        metric_be = metric_be_registry.registry[i];
 
         if (metric_be->id == id) {
             return metric_be;
         }
     }
+
     return NULL;
 }
