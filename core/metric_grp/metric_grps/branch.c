38175721b9a0282c4beea667deddd30be531a1f5
Matthew Penney <matt@matthewpenney.net>
2026-05-22 15:30:18 +0100

    Disable metrics in the BRANCH group since they cause problems on ARM

--- core/metric_grp/metric_grps/branch.c
+++ core/metric_grp/metric_grps/branch.c
@@ -2,15 +2,15 @@
 
 static metric_id_t metrics[] = {
     METRIC_CPU_CYCLES,
-    METRIC_BRANCH_INSTRUCTIONS,
+    //METRIC_BRANCH_INSTRUCTIONS,
     METRIC_BRANCH_MISPREDICTIONS,
-    METRIC_BRANCH_MISPRED_RATE,
+    //METRIC_BRANCH_MISPRED_RATE,
 };
 
 static metric_grp_t mg = {
     .name = "BRANCH",
     .backend = METRIC_BE_PERF,
-    .n_metrics = 4,
+    .n_metrics = 2,
     .metrics = metrics,
 };
 
